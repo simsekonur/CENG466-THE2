61a2d56b4c09bab9b8b8066259aba993560d56c9
Onur Şimşek <38193815+onurtgmn@users.noreply.github.com>
2019-12-03 15:32:12 +0300

    solution for 2 is true

--- THE2_images/the2_part1.m
+++ THE2_images/the2_part1.m
@@ -5,38 +5,98 @@ A3 = imread('A3.png');
 
 [M1,N1] = size(A1);
 [M2,N2] = size(A2);
-[M3,N3] = size(A3);
+[M3,N3,Z3] = size(A3);
+
+A1_t = fft2(A1);
+%figure, imshow(fftshift(abs(A1_t)), [0, 40000])
+
+A2_t = fft2(A2);
+%figure, imshow(fftshift(abs(A2_t)), [0, 40000])
+
+A3_t = fft2(A3);
+%figure, imshow(fftshift(abs(A3_t)), [0, 40000])
 
 
-
-A1_fs = fftshift(fft2(A1));
 filter1 = ones(M1,N1);
 
-dist1min = 280;
-dist1max = 320;
-dist2min = 65;
-dist2max = 85;
+center_x = (M1-1)/2;
+center_y = (N1-1)/2;
 
-midi=(M1-1)/2;
-midj=(N1-1)/2;
 
-%maxdistance = 0 ;
 
-for i=1:M1
-    for j=1:N1
-        
-        % distance from image center to pixel i, j
-        distance = ((i - midi).^2 + (j - midj).^2).^0.5;
-        
-        
-        if (distance >337)
-            filter1(i, j) = 0;
-        end
+for x=1:M1
+   for y=1:N1
        
-    end
+       distance = sqrt((x-center_x)^2+(y-center_y)^2);
+       if(distance > 24000)
+           filter1(x,y)=0;
+       end
+   end 
 end
 
-A1_output = ifft2(ifftshift(A1_fs .* filter1));
+filter1 = ifftshift(filter1);
+A1_t = A1_t .* filter1;
+
+A1_new = abs(ifft2(A1_t));
+A1_new = A1_new / 255.0;
+A1_new = imadjust(A1_new);
+figure ,imshow(A1_new)
 
 
-figure ,imshow(uint8(real(A1_output)));
+
+
+filter2 = ones(M2,N2);
+
+center_x = (M2-1)/2;
+center_y = (N2-1)/2;
+
+
+
+for x=1:M2
+   for y=1:N2
+       distance = sqrt((x-center_x)^2+(y-center_y)^2);
+       if(distance > 250)
+           filter2(x,y)=0;
+       end
+   end 
+end
+
+filter2 = ifftshift(filter2);
+A2_t = A2_t .* filter2;
+
+A2_new = abs(ifft2(A2_t));
+A2_new = A2_new / 255.0;
+A2_new = imadjust(A2_new);
+figure , imshow(A2_new)
+
+
+
+
+filter3 = ones(M3,N3,Z3);
+
+center_x = (M3-1)/2;
+center_y = (N3-1)/2;
+center_z = (Z3-1)/2;
+
+
+for x=1:M3
+   for y=1:N3
+        for z=1:Z3
+            distance = sqrt((x-center_x)^2+(y-center_y)^2);
+            if(distance > 95)
+                filter3(x,y,z)=0;
+            end
+        end 
+   end 
+end
+
+filter3 = ifftshift(filter3);
+A3_t = A3_t .* filter3;
+
+A3_new = abs(ifft2(A3_t));
+A3_new = A3_new / 255.0;
+
+figure , imshow(A3_new)
+
+
+

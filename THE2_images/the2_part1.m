36d4e8648f9b09c16fabb42275efdee55222afbc
Onur Şimşek <38193815+onurtgmn@users.noreply.github.com>
2019-12-03 09:32:56 +0300

    Looking very similar

--- THE2_images/the2_part1.m
+++ THE2_images/the2_part1.m
@@ -7,27 +7,36 @@ A3 = imread('A3.png');
 [M2,N2] = size(A2);
 [M3,N3] = size(A3);
 
-A1_t = fft2(A1);
 
-center_x = round(M1/2);
-center_y = round(N1/2);
-filter1 = ones(M1, N1);
-for x = 1:M1
-   for y = 1:N1
-      distance = sqrt((x-center_x)^2 + (y-center_y)^2);
-      if((distance > 63 && distance < 85) || (distance > 280 && distance < 320))
-         filter1(x, y) = 0;
-      end
-   end
+
+A1_fs = fftshift(fft2(A1));
+filter1 = ones(M1,N1);
+
+dist1min = 280;
+dist1max = 320;
+dist2min = 65;
+dist2max = 85;
+
+midi=(M1-1)/2;
+midj=(N1-1)/2;
+
+%maxdistance = 0 ;
+
+for i=1:M1
+    for j=1:N1
+        
+        % distance from image center to pixel i, j
+        distance = ((i - midi).^2 + (j - midj).^2).^0.5;
+        
+        
+        if (distance >337)
+            filter1(i, j) = 0;
+        end
+       
+    end
 end
-filter1 = ifftshift(filter1); % shift back
-A1_t = A1_t .* filter1; 
-A1_new = ifft2(A1_t);
 
-% Transformed values should be mapped.
-A1_new = A1_new/255.0; % Adjust the color range to [0, 1]
-% Map the gray levels to the range [0.0, 1.0]
-A1_new = imadjust(A1_new, [min(A1_new(:)); max(A1_new(:))], [0.0; 1.0]);
+A1_output = ifft2(ifftshift(A1_fs .* filter1));
 
-figure, imshow(A1_new); % Display the output image 
 
+figure ,imshow(uint8(real(A1_output)));
